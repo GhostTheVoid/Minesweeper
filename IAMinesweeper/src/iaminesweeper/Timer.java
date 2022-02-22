215a8364d6cacfb4ca9f12a675fe7f3795ae4443
Ghost <67445574+GhostTheVoid@users.noreply.github.com>
2022-02-22 08:02:00 -0600

    2022-02-22

--- IAMinesweeper/src/iaminesweeper/Timer.java
+++ IAMinesweeper/src/iaminesweeper/Timer.java
@@ -1,6 +1,10 @@
 /** Required package class namespace */
 package iaminesweeper;
 
+import javax.swing.JLabel;
+import tools.Directions;
+import tools.GameCharacter;
+
  
 /**
  * Timer.java - description
@@ -9,43 +13,17 @@ package iaminesweeper;
  * @author Marissa Rowles
  * @since 16-Feb-2022
  */
-public class Timer {
+public class Timer extends GameCharacter{
 
     /**
      * Default constructor, set class properties
      */
-    public Timer() {
-        
+    public Timer(JLabel timerLabel) {
+        super(timerLabel, Constants.CELL_MOVE_AMOUNT, 
+                Constants.CELL_TIMER_DELAY);
     }
-     
-    /**
-     * String representation of this object
-     *
-     * @return The object represented as a String
-     */
+
     @Override
-    public String toString() {
-        return "Timer: " + super.toString();
-    }
-   
-    /**
-     * Deep comparison, determines if two objects are "equal" in this context
-     *
-     * @param object the object to compare to
-     * @return the objects are "equal" (true) or not (false)
-     */
-    @Override
-    public boolean equals(Object object) {
-        return super.equals(object);
-    }
-       
-    /**
-     * a Deep clone, creates a duplicate object using new memory
-     *
-     * @return a "clone" of the object using new memory
-     */
-    @Override
-    public Timer clone() {
-        return this;
+    public void action() {
     }
 }
