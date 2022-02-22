d7bde38cb74f373cef72701b8de8d758ed69f6f7
Ghost <67445574+GhostTheVoid@users.noreply.github.com>
2022-02-22 08:02:34 -0600

    2022-02-22

--- IAMinesweeper/src/iaminesweeper/Constants.java
+++ IAMinesweeper/src/iaminesweeper/Constants.java
@@ -11,27 +11,51 @@ package iaminesweeper;
  */
 public class Constants {
 
-    public final static String SPRITE_SHEET    = "/media/minesweeper.png";
+    public final static int    CELL_MOVE_AMOUNT         = 0;
     
-    public final static String NUM_1_TAG = "num1";
-    public final static String NUM_2_TAG = "num2";
-    public final static String NUM_3_TAG = "num3";
-    public final static String NUM_4_TAG = "num4";
-    public final static String NUM_5_TAG = "num5";
-    public final static String NUM_6_TAG = "num6";
-    public final static String NUM_7_TAG = "num7";
-    public final static String NUM_8_TAG = "num8";
-    public final static String NUM_9_TAG = "num9";
-    public final static String NUM_0_TAG = "num0";
-    public final static String NUM_DASH_TAG = "numDash";
-    public final static String NUM_BLANK_TAG = "numBlank";
+    public final static int    GAME_TIMER_DELAY         = 1000;
+    public final static int    CELL_TIMER_DELAY         = 100;
     
-    public final static String DEFAULT_FACE_TAG = "defaultFace";
-    public final static String RESET_FACE_TAG = "resetFace";
-    public final static String CLICK_FACE_TAG = "clickFace";
-    public final static String WIN_FACE_TAG = "winFace";
-    public final static String LOSE_FACE_TAG = "loseFace";
+    public final static int    CELL_ANIMATION_DELAY        = 1000;
     
+    public final static String PLAYER_DATA_FILE    = "/media/data.txt";
+    public final static String SETTINGS_DATA_FILE  = "/media/settings.txt";
+    public final static String SPRITE_SHEET        = "/media/minesweeper.png";
     
-    
+    // Num
+    public final static String NUM_1_TAG                = "num1";
+    public final static String NUM_2_TAG                = "num2";
+    public final static String NUM_3_TAG                = "num3";
+    public final static String NUM_4_TAG                = "num4";
+    public final static String NUM_5_TAG                = "num5";
+    public final static String NUM_6_TAG                = "num6";
+    public final static String NUM_7_TAG                = "num7";
+    public final static String NUM_8_TAG                = "num8";
+    public final static String NUM_9_TAG                = "num9";
+    public final static String NUM_0_TAG                = "num0";
+    public final static String NUM_DASH_TAG             = "numDash";
+    public final static String NUM_BLANK_TAG            = "numBlank";
+    // Face
+    public final static String FACE_DEFAULT_TAG         = "defaultFace";
+    public final static String FACE_RESET_TAG           = "resetFace";
+    public final static String FACE_CLICK_TAG           = "clickFace";
+    public final static String FACE_WIN_TAG             = "winFace";
+    public final static String FACE_LOSE_TAG            = "loseFace";
+    // Cells
+    public final static String CELL_1_TAG               = "cell1";
+    public final static String CELL_2_TAG               = "cell2";
+    public final static String CELL_3_TAG               = "cell3";
+    public final static String CELL_4_TAG               = "cell4";
+    public final static String CELL_5_TAG               = "cell5";
+    public final static String CELL_6_TAG               = "cell6";
+    public final static String CELL_7_TAG               = "cell7";
+    public final static String CELL_8_TAG               = "cell8";
+    public final static String CELL_DEFAULT_TAG         = "defaultCell";
+    public final static String CELL_CLICK_TAG           = "clickCell";
+    public final static String CELL_FLAG_TAG            = "flagCell";
+    public final static String CELL_UNKNOWN_TAG         = "unknownCell";
+    public final static String CELL_UNKNOWN_CLICK_TAG   = "unknownClickCell";
+    public final static String CELL_BOMB_TAG            = "bombCell";
+    public final static String CELL_BOMB_HIT_TAG        = "bombHitCell";
+    public final static String CELL_BOMB_CHECKED_TAG    = "bombCheckedCell";
 }
