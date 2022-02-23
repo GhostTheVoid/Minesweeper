3616909504cd34931eec22465abb6d024dcf8403
Ghost <67445574+GhostTheVoid@users.noreply.github.com>
2022-02-23 10:13:05 -0600

    2022-02-23

--- IAMinesweeper/src/iaminesweeper/Constants.java
+++ IAMinesweeper/src/iaminesweeper/Constants.java
@@ -11,15 +11,17 @@ package iaminesweeper;
  */
 public class Constants {
 
-    public final static int    CELL_MOVE_AMOUNT         = 0;
+    public final static int    TIMER_MOVE_AMOUNT         = 0;
     public final static int    FACE_MOVE_AMOUNT         = 0;
+    public final static int    CELL_MOVE_AMOUNT         = 0;
     
-    public final static int    GAME_TIMER_DELAY         = 1000;
-    public final static int    CELL_TIMER_DELAY         = 100;
+    public final static int    TIMER_TIMER_DELAY         = 1000;
     public final static int    FACE_TIMER_DELAY         = 100;
+    public final static int    CELL_TIMER_DELAY         = 100;
     
-    public final static int    CELL_ANIMATION_DELAY        = 1000;
-    public final static int    FACE_ANIMATION_DELAY        = 1000;
+    public final static int    TIMER_ANIMATION_DELAY     = 1000;
+    public final static int    FACE_ANIMATION_DELAY     = 1000;
+    public final static int    CELL_ANIMATION_DELAY     = 1000;
     
     public final static String PLAYER_DATA_FILE    = "/media/data.txt";
     public final static String SETTINGS_DATA_FILE  = "/media/settings.txt";
@@ -39,11 +41,11 @@ public class Constants {
     public final static String NUM_DASH_TAG             = "numDash";
     public final static String NUM_BLANK_TAG            = "numBlank";
     // Face
-    public final static String FACE_DEFAULT_TAG         = "defaultFace";
-    public final static String FACE_RESET_TAG           = "resetFace";
-    public final static String FACE_CLICK_TAG           = "clickFace";
-    public final static String FACE_WIN_TAG             = "winFace";
-    public final static String FACE_LOSE_TAG            = "loseFace";
+    public final static String FACE_DEFAULT_TAG         = "faceDefault";
+    public final static String FACE_RESET_TAG           = "faceReset";
+    public final static String FACE_CLICK_TAG           = "faceClick";
+    public final static String FACE_WIN_TAG             = "faceWin";
+    public final static String FACE_LOSE_TAG            = "faceLose";
     // Cells
     public final static String CELL_1_TAG               = "cell1";
     public final static String CELL_2_TAG               = "cell2";
@@ -53,12 +55,12 @@ public class Constants {
     public final static String CELL_6_TAG               = "cell6";
     public final static String CELL_7_TAG               = "cell7";
     public final static String CELL_8_TAG               = "cell8";
-    public final static String CELL_DEFAULT_TAG         = "defaultCell";
-    public final static String CELL_CLICK_TAG           = "clickCell";
-    public final static String CELL_FLAG_TAG            = "flagCell";
-    public final static String CELL_UNKNOWN_TAG         = "unknownCell";
-    public final static String CELL_UNKNOWN_CLICK_TAG   = "unknownClickCell";
-    public final static String CELL_BOMB_TAG            = "bombCell";
-    public final static String CELL_BOMB_HIT_TAG        = "bombHitCell";
-    public final static String CELL_BOMB_CHECKED_TAG    = "bombCheckedCell";
+    public final static String CELL_DEFAULT_TAG         = "cellDefault";
+    public final static String CELL_CLICK_TAG           = "cellClick";
+    public final static String CELL_FLAG_TAG            = "cellFlag";
+    public final static String CELL_UNKNOWN_TAG         = "cellUnknown";
+    public final static String CELL_UNKNOWN_CLICK_TAG   = "cellUnknownClick";
+    public final static String CELL_BOMB_TAG            = "cellBomb";
+    public final static String CELL_BOMB_HIT_TAG        = "cellBombHit";
+    public final static String CELL_BOMB_CHECKED_TAG    = "cellBombChecked";
 }
--- IAMinesweeper/src/iaminesweeper/Timer.java
+++ IAMinesweeper/src/iaminesweeper/Timer.java
@@ -1,29 +1,92 @@
 /** Required package class namespace */
 package iaminesweeper;
 
+import collections.LinkedList;
 import javax.swing.JLabel;
-import tools.Directions;
-import tools.GameCharacter;
+import tools.Animation;
+import tools.GameObject;
 
  
 /**
- * Timer.java - description
+ * Timer.java - representation of a timer in minesweeper
  *
  * -----------------------
  * @author Marissa Rowles
  * @since 16-Feb-2022
  */
-public class Timer extends GameCharacter{
+public class Timer extends GameObject{
+    
+    
 
     /**
      * Default constructor, set class properties
+     * 
+     * @param timerLabel
+     * @param settings
      */
-    public Timer(JLabel timerLabel) {
-        super(timerLabel, Constants.CELL_MOVE_AMOUNT, 
-                Constants.CELL_TIMER_DELAY);
+    public Timer(JLabel timerLabel, LinkedList<String> settings) {
+        super(timerLabel);
+        setAnimations(timerLabel, settings); // build all animations
     }
-
-    @Override
-    public void action() {
+    
+    /**
+     * Set up all the animations for this character
+     * 
+     * @param label the label to associate the animation with
+     * @param settings the list of animation settings
+     */
+    private void setAnimations(JLabel label, LinkedList<String> settings) {
+        String sheet = Constants.SPRITE_SHEET;
+        int    delay = Constants.TIMER_ANIMATION_DELAY; 
+        String tag   = Constants.NUM_1_TAG;        
+        Animation num1 = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        tag = Constants.NUM_2_TAG;        
+        Animation num2 = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        tag = Constants.NUM_3_TAG;        
+        Animation num3 = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        tag = Constants.NUM_4_TAG;        
+        Animation num4 = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);        
+        tag = Constants.NUM_5_TAG;        
+        Animation num5 = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        tag = Constants.NUM_6_TAG;        
+        Animation num6 = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        tag = Constants.NUM_7_TAG;        
+        Animation num7 = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        tag = Constants.NUM_8_TAG;        
+        Animation num8 = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        tag = Constants.NUM_9_TAG;        
+        Animation num9 = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        tag = Constants.NUM_0_TAG;        
+        Animation num0 = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        tag = Constants.NUM_DASH_TAG;        
+        Animation numDash = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        tag = Constants.NUM_BLANK_TAG;        
+        Animation numBlank = Animator.getAnimation(sheet, label, 
+                                                       delay, settings, tag);
+        LinkedList<Animation> timerAnimations = new LinkedList<>(); 
+        timerAnimations.add(num1);
+        timerAnimations.add(num2);
+        timerAnimations.add(num3);
+        timerAnimations.add(num4);
+        timerAnimations.add(num5);
+        timerAnimations.add(num6);
+        timerAnimations.add(num7);
+        timerAnimations.add(num8);
+        timerAnimations.add(num9);
+        timerAnimations.add(num0);
+        timerAnimations.add(numDash);
+        timerAnimations.add(numBlank);
+        sprite.setAnimations(timerAnimations);             
     }
 }
